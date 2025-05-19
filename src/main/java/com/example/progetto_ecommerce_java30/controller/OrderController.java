0900fcf30a0e6a2eb8ea12f3f08741367b98e947
DjGuruge <djguruge@hotmail.it>
2025-05-19 11:44:04 +0200

    crud implementation on OrderController and OrderService

--- src/main/java/com/example/progetto_ecommerce_java30/controller/OrderController.java
+++ src/main/java/com/example/progetto_ecommerce_java30/controller/OrderController.java
@@ -1,9 +1,14 @@
 package com.example.progetto_ecommerce_java30.controller;
 
+import com.example.progetto_ecommerce_java30.entity.OrderEntity;
+import com.example.progetto_ecommerce_java30.entity.ShoppingCartEntity;
 import com.example.progetto_ecommerce_java30.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/order")
@@ -11,4 +16,32 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    @GetMapping
+    public List<OrderEntity> allOrder(){
+        return orderService.allOrder();
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderEntity> addOrder(@RequestBody OrderEntity newOrder){
+        return ResponseEntity.ok(orderService.addOrder(newOrder));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderEntity> getOrderbyId(@PathVariable Long id){
+        Optional<OrderEntity> foundOrder = orderService.getOrderById(id);
+
+        return foundOrder
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteShoppingCartById(@PathVariable Long id){
+        orderService.deleteOrderById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
